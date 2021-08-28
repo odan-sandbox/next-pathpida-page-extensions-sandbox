e2af0cff96f6c103308aac918477b55fe98ecb5b
odan <odan3240@gmail.com>
2021-08-29 01:54:24 +0900

    rename

--- lib/$path.ts
+++ lib/$path.ts
@@ -4,6 +4,9 @@ export const pagesPath = {
   hello_page: {
     $url: (url?: { hash?: string }) => ({ pathname: '/hello.page' as const, hash: url?.hash })
   },
+  index_page: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/index.page' as const, hash: url?.hash })
+  },
   post: {
     [id]_page: {
       $url: (url?: { hash?: string }) => ({ pathname: '/post/[id].page' as const, hash: url?.hash })
@@ -11,8 +14,7 @@ export const pagesPath = {
   },
   test: {
     $url: (url?: { hash?: string }) => ({ pathname: '/test' as const, hash: url?.hash })
-  },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+  }
 }
 
 // prettier-ignore
