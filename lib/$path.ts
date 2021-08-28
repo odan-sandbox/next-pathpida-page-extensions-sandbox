8f700d47134274e0769f64b8a1a082731772ed33
odan <odan3240@gmail.com>
2021-08-29 01:48:27 +0900

    add mdx

--- lib/$path.ts
+++ lib/$path.ts
@@ -1,12 +1,17 @@
 /* eslint-disable */
 // prettier-ignore
 export const pagesPath = {
+  index_page: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/index.page' as const, hash: url?.hash })
+  },
   post: {
     [id]_page: {
       $url: (url?: { hash?: string }) => ({ pathname: '/post/[id].page' as const, hash: url?.hash })
     }
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+  test: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/test' as const, hash: url?.hash })
+  }
 }
 
 // prettier-ignore
--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+const withMDX = require("@next/mdx")({ extension: /\.mdx?$/ });
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withMDX({
   reactStrictMode: true,
-  pageExtensions: ["page.tsx"]
-}
+  pageExtensions: ["page.tsx", "md", "mdx"],
+});
